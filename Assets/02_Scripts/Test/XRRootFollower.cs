d25e4ddbb083e22e0e13825bf3e04c97ee60a936
godozi <tpdjq1900@gmail.com>
2025-07-10 17:27:23 +0900

    bb

--- Assets/02_Scripts/Test/XRRootFollower.cs
+++ Assets/02_Scripts/Test/XRRootFollower.cs
@@ -7,13 +7,18 @@ public class XRRootFollower : NetworkBehaviour
 {
     public Transform xrOrigin;
 
-    void LateUpdate()
+    public Transform cameraOffset;
+
+    public override void FixedUpdateNetwork()
     {
         if (HasInputAuthority && xrOrigin != null)
         {
-            transform.position = xrOrigin.position;
-            transform.rotation = Quaternion.Euler(0, xrOrigin.eulerAngles.y, 0);
-            Debug.Log($"[XRRootFollower] Cube 위치 갱신됨: {transform.position}");
+            if (cameraOffset != null)
+            {
+                transform.position = cameraOffset.position;
+                transform.rotation = Quaternion.Euler(0, cameraOffset.eulerAngles.y, 0);
+            }
+            Debug.Log($"카메라 오프셋 위치: {cameraOffset.position}");
         }
     }
 }
