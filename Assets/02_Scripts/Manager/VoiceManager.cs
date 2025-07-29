3b574e684879b83b75b57f77ca6ac344a3d9e0ec
godozi <tpdjq1900@gmail.com>
2025-07-29 12:12:28 +0900

    VT

--- Assets/02_Scripts/Manager/VoiceManager.cs
+++ Assets/02_Scripts/Manager/VoiceManager.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 using Photon.Voice.Unity;
 using Photon.Realtime;
+
 public class VoiceManager : MonoBehaviour
 {
     public VoiceConnection VoiceConnection { get; private set; }
+    public Recorder recorder { get; private set; }
 
     private void Awake()
     {
         VoiceConnection = GetComponent<VoiceConnection>();
+        recorder = GetComponent<Recorder>();
     }
-
+    private void Start()
+    {
+        VoiceConnection.SpeakerLinked += OnSpeakerLinked;
+    } 
     public void ConnectToVoiceRoom(string roomName)
     {
         if (!VoiceConnection.Client.IsConnected)
@@ -27,4 +33,18 @@ public class VoiceManager : MonoBehaviour
             });
         }
     }
+    private void OnSpeakerLinked(Speaker speaker)
+    {
+        Debug.Log($"[VoiceManager] Speaker 연결됨: {speaker.name}");
+        // 필요시 speaker.AudioSource 조정 가능
+    }
+    public void SetVoice(bool enabled)
+    {
+        if (recorder != null)
+        {
+            recorder.TransmitEnabled = enabled;
+            recorder.RecordingEnabled = enabled;
+        }
+    }
+
 }
