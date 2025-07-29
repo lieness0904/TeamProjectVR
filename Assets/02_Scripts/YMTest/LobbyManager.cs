35e0881c024a81175748f0e33f55e762070a541a
godozi <tpdjq1900@gmail.com>
2025-07-29 10:11:59 +0900

    gogo

--- Assets/02_Scripts/YMTest/LobbyManager.cs
+++ Assets/02_Scripts/YMTest/LobbyManager.cs
@@ -34,7 +34,7 @@ public class LobbyManager : MonoBehaviour, INetworkRunnerCallbacks
         await runner.StartGame(new StartGameArgs()
         {
             GameMode = GameMode.AutoHostOrClient,
-            SessionName = "Jeju_Lobby1",
+            SessionName = "Jeju_Lobby123",
         });
     }
 
