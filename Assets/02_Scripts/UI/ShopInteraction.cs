889c327ad8a29dcc681a3c512c12f24ff321960c
oseo <dudtj8787@gmail.com>
2025-07-29 17:52:57 +0900

    0729 shopUI
    shopUI업데이트

--- Assets/02_Scripts/UI/ShopInteraction.cs
+++ Assets/02_Scripts/UI/ShopInteraction.cs
@@ -3,51 +3,80 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ShopInteraction : MonoBehaviour, IInteractable
-{
-    public GameObject shopButtonUI;
-    public GameObject shopUI;
-    public ShopManager shopManager;
-    [SerializeField] private float closeDistance = 2f;
+//public class ShopInteraction : MonoBehaviour, IInteractable
+//{
+//    public GameObject shopButtonUI;
+//    public GameObject shopUI;
+//    public ShopManager shopManager;
+//    [SerializeField] private float closeDistance = 2f;
 
-    private Transform targetPlayer;
+//    private Transform targetPlayer;
+//    private void Start()
+//    {
+//        if (shopButtonUI != null)
+//            shopButtonUI.SetActive(false);
+//    }
+//    public void Interact(Transform interactor)
+//    {
+//        float dist = Vector3.Distance(transform.position, interactor.position);
+//        if (dist <= closeDistance && shopButtonUI != null && !shopUI.activeSelf)
+//        {
+//            targetPlayer = interactor;
+//            shopButtonUI.SetActive(true);
+//            StartCoroutine(CheckDistanceRoutine());
+//        }
+//    }
+//    public float GetInteractRange()
+//    {
+//        return 3f;
+//    }
+//    public void OnShopButtonClick()
+//    {
+//        shopManager.OpenShop();
+//        shopButtonUI.SetActive(false);
+//    }
+//    private IEnumerator CheckDistanceRoutine()
+//    {
+//        while (shopButtonUI.activeSelf)
+//        {
+//            if (targetPlayer == null) yield break;
+
+//            float dist = Vector3.Distance(transform.position, targetPlayer.position);
+//            if (dist > closeDistance)
+//            {
+//                shopButtonUI.SetActive(false);
+//                yield break;
+//            }
+//            yield return new WaitForSeconds(0.2f);
+//        }
+//    }
+//}
+
+public class ShopInteraction : MonoBehaviour
+{
+    public GameObject shopButtonUI; // 버튼 오브젝트
+    public GameObject shopUI;       // Shop 패널 오브젝트
+    public ShopManager shopManager; // 상점 로직 관리자
+
     private void Start()
     {
         if (shopButtonUI != null)
-            shopButtonUI.SetActive(false);
-    }
-    public void Interact(Transform interactor)
-    {
-        float dist = Vector3.Distance(transform.position, interactor.position);
-        if (dist <= closeDistance && shopButtonUI != null && !shopUI.activeSelf)
-        {
-            targetPlayer = interactor;
             shopButtonUI.SetActive(true);
-            StartCoroutine(CheckDistanceRoutine());
-        }
+
+        if (shopUI != null)
+            shopUI.SetActive(false);
     }
-    public float GetInteractRange()
-    {
-        return 3f;
-    }
+
+    // 버튼에 연결될 함수
     public void OnShopButtonClick()
     {
-        shopManager.OpenShop();
-        shopButtonUI.SetActive(false);
-    }
-    private IEnumerator CheckDistanceRoutine()
-    {
-        while (shopButtonUI.activeSelf)
-        {
-            if (targetPlayer == null) yield break;
+        if (shopManager != null)
+            shopManager.OpenShop();
 
-            float dist = Vector3.Distance(transform.position, targetPlayer.position);
-            if (dist > closeDistance)
-            {
-                shopButtonUI.SetActive(false);
-                yield break;
-            }
-            yield return new WaitForSeconds(0.2f);
-        }
+        if (shopUI != null)
+            shopUI.SetActive(true);
+
+        if (shopButtonUI != null)
+            shopButtonUI.SetActive(false);
     }
 }
