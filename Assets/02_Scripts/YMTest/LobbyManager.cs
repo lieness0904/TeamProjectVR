9d0eaba4a9d40e1d088d342b8fa308cea2cf8cc1
YJKor <tpdjq1900@gmail.com>
2025-08-04 14:55:20 +0900

    gg

--- Assets/02_Scripts/YMTest/LobbyManager.cs
+++ Assets/02_Scripts/YMTest/LobbyManager.cs
@@ -45,7 +45,7 @@ public class LobbyManager : MonoBehaviour, INetworkRunnerCallbacks
         {
             GameMode = GameMode.AutoHostOrClient,
 
-            SessionName = "Jeju_Lobby111",
+            SessionName = "Jeju_Lobby1111",
             SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
 
         });
